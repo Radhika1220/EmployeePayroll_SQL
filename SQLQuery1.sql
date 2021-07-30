b329723212030a3cd9e9b009b9e70a4a3a6b7925
Radhika <radhika.shankar1220@gmail.com>
2021-07-30 15:28:35 +0530

    [radhika] alter and update the data in table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -17,7 +17,7 @@ emailId varchar(20)
 
 Insert into employee_payroll(name,salary,startDate,emailId) values ('Radhika',35000,'2021-06-21','radz@yahoo.com'),('Priya',50000,'2021-01-15','priya@yahoo.com');
 Insert into employee_payroll values ('Arun',60000,'2021-03-13','arun12@gmail.com'),('Kishore',55000,'2021-05-23','ksihore@yahoo.com');
-
+Insert into employee_payroll values ('Ajoy',70000,'2021-05-24','ajoyjones@yahoo.com');
 ----------UC4--------------
 select * from employee_payroll;
 
@@ -25,3 +25,11 @@ select * from employee_payroll;
 select name,salary from employee_payroll where name='Kishore';
 -------------UC5----------------
 select name,salary from employee_payroll where startDate between ('2021-05-01') and getdate();
+
+
+-------------UC6----------
+alter table employee_payroll add Gender char(1);
+-----------UC6-------------
+update employee_payroll set Gender='M' where name='Kishore' or name='Arun' or name='Ajoy';
+
+update employee_payroll set Gender='F' where name='Radhika' or name='Priya';
