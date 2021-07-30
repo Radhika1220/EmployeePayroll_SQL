b0a4ed88846704fb1fb5da38bd05ba5ca439ade4
Radhika <radhika.shankar1220@gmail.com>
2021-07-30 14:59:53 +0530

    [Radhika] insert query

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,7 +3,7 @@
 create database payroll_service;
 
 use payroll_service;
-
+---------UC2-------------
 create table employee_payroll
 (
 empId int identity(1,1) primary key,
@@ -12,4 +12,9 @@ salary float,
 startDate date,
 emailId varchar(20)
 );
+
+----------UC3-------------
+
+Insert into employee_payroll(name,salary,startDate,emailId) values ('Radhika',35000,'2021-06-21','radz@yahoo.com'),('Priya',50000,'2021-01-15','priya@yahoo.com');
+Insert into employee_payroll values ('Arun',60000,'2021-03-13','arun12@gmail.com'),('Kishore',55000,'2021-05-23','ksihore@yahoo.com');
 
