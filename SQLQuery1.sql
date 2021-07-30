d37502c92c2b64dd4b56e9dd15ca04862c699f40
Radhika <radhika.shankar1220@gmail.com>
2021-07-30 14:55:00 +0530

    [Radhika] create table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,12 @@ create database payroll_service;
 
 use payroll_service;
 
+create table employee_payroll
+(
+empId int identity(1,1) primary key,
+name varchar(20) not null,
+salary float,
+startDate date,
+emailId varchar(20)
+);
+
